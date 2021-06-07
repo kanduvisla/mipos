62078cd953f9a1fee42bdd6d17f52b58dd025d76
Giel Berkers <info@gielberkers.com>
2021-06-07 14:37:40 +0100

    Make LED blinking

--- mipos.c
+++ mipos.c
@@ -1,22 +1,37 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <bcm2835.h>
+#include <alsa/asoundlib.h>
+
+#define LED_POWER RPI_GPIO_P1_11	// Power LED
+#define LED_CLOCK RPI_GPIO_P1_12	// Midi Clock Indicator LED
 
 static volatile sig_atomic_t keep_running = 1;
 
 static void sig_handler(int _)
 {
-    (void)_;
-    keep_running = 0;
+	(void)_;
+	keep_running = 0;
 }
 
 int main(void)
 {
-    signal(SIGINT, sig_handler);
+	signal(SIGINT, sig_handler);
 
-    while (keep_running)
-        puts("Still running...");
+	// Initialize BCM2835:
+	bcm2835_init();
+	bcm2835_gpio_fsel(LED_POWER, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(LED_CLOCK, BCM2835_GPIO_FSEL_OUTP);
 
-    puts("Stopped by signal `SIGINT'");
-    return EXIT_SUCCESS;
+	while (keep_running) {
+		// check if midi device is available
+		// If not, the power LED keeps blinking
+		bcm2835_gpio_write(LED_POWER, HIGH);
+		bcm2835_delay(500);
+		bcm2835_gpio_write(LED_POWER, LOW);
+		bcm2835_delay(500);
+	}
+
+    return 0;
 }
