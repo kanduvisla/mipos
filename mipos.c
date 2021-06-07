04c5ac591e29d1c9a5b5a3085f88302e58c60ada
Giel Berkers <info@gielberkers.com>
2021-06-07 15:04:40 +0100

    Make LED blink on click track

--- mipos.c
+++ mipos.c
@@ -6,9 +6,18 @@
 
 #define LED_POWER RPI_GPIO_P1_11	// Power LED
 #define LED_CLOCK RPI_GPIO_P1_12	// Midi Clock Indicator LED
+#define CLOCK_DIV 12			// 12 clock ticks per 8th note
+#define PULSE_LEN 15			// 15 ms pulse
 
 static volatile sig_atomic_t keep_running = 1;
 static volatile sig_atomic_t is_midi_connected = 0;
+
+typedef enum play_state
+{
+	play_state_stopped,
+	play_state_playing,
+}
+play_state;
 
 static void sig_handler(int _)
 {
@@ -29,6 +38,9 @@ int main(int argc, char **argv)
 	bcm2835_gpio_fsel(LED_POWER, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(LED_CLOCK, BCM2835_GPIO_FSEL_OUTP);
 
+	play_state state = play_state_stopped;
+	uint8_t ticks = 0;
+
 	while (keep_running) {
 		// check if midi device is available
 		// If not, the power LED keeps blinking
@@ -43,9 +55,57 @@ int main(int argc, char **argv)
 			bcm2835_gpio_write(LED_POWER, HIGH);
 			bcm2835_delay(500);
 		}
+
+		// MIDI Device is detected
+		uint8_t status;
+		snd_rawmidi_read(midiin, &status, 1);
+
+		if(status & 0xF0)
+		{
+			switch(status)
+			{
+				case 0xFA: // start
+				{
+					ticks = 0; // reset clock
+					state = play_state_playing;
+					break;
+				}
+				case 0xFB: // continue
+				{
+					state = play_state_playing;
+					break;
+				}
+				case 0xFC: // stop
+				{
+					state = play_state_stopped;
+					break;
+				}
+				case 0xF8: // clock tick
+				{
+					if(ticks % CLOCK_DIV == 0)
+					{
+						ticks = 0; // reset clock
+						if(state == play_state_playing)
+						{
+							bcm2835_gpio_write(LED_CLOCK, HIGH);
+							bcm2835_delay(PULSE_LEN);
+							bcm2835_gpio_write(LED_CLOCK, LOW);
+						}
+					}
+
+					if(state == play_state_playing)
+						ticks++;
+					break;
+				}
+				default:
+					break;
+			}
+		}
 	}
 
+	// Make sure LED's are turned off
 	bcm2835_gpio_write(LED_POWER, LOW);
+	bcm2835_gpio_write(LED_CLOCK, LOW);
 
 	return 0;
 }
